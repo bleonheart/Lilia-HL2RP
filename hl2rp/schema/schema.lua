c156c724e47d928fdd0d8ee9e1622cba728624cc
Leonheart <33399712+bleonheart@users.noreply.github.com>
2024-05-20 18:11:22 +0100

    Updated Several Items

--- hl2rp/schema/schema.lua
+++ hl2rp/schema/schema.lua
@@ -1,7 +1,18 @@
-----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--[[--
+    Schema configuration options.
+
+    This is meant to define the Schema's default information.
+]]
+-- @configurations Schema
+
+--- A list of schema fields that define the gamemode you are making.
+-- These options are defined by the 'SCHEMA' table.
+-- @realm shared
+-- @table Configurations
+-- @field name Controls the name of the schema | **string**
+-- @field author Specifies the author of the schema | **string**
+-- @field desc Defines the description of the schema | **string**
 SCHEMA.name = "Half-Life 2 Roleplay"
---------------------------------------------------------------------------------------------------------
-SCHEMA.author = "@liliaplayer"
---------------------------------------------------------------------------------------------------------
-SCHEMA.desc = "Under the Universal Union's rule."
---------------------------------------------------------------------------------------------------------
+SCHEMA.author = "76561198312513285"
+SCHEMA.discord = "@liliaplayer"
+SCHEMA.desc = "Under the Universal Union's rule"
